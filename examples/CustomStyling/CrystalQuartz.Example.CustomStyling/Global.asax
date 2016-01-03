d335f8766ed102ee9a3dd1858e399c223341bd1c
EugeneGuryanov <eguryanov@codereign.net>
2016-01-03 17:37:49 +0300

    Added custom styling example.

---
+++ examples/CustomStyling/CrystalQuartz.Example.CustomStyling/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CrystalQuartz.Example.CustomStyling.MvcApplication" Language="C#" %>
---
+++ examples/CustomStyling/CrystalQuartz.Example.CustomStyling/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CrystalQuartz.Example.CustomStyling.Startup))]
+namespace CrystalQuartz.Example.CustomStyling
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
