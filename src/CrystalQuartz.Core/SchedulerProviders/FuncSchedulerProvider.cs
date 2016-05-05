5a7c7d10646f0c636ae7867a6c3088089952416a
guryanovev <guryanovev@gmail.com>
2016-05-05 17:17:56 +0300

    Added func scheduler provider and Owin configuration overloads.

---
+++ src/CrystalQuartz.Core/SchedulerProviders/FuncSchedulerProvider.cs
@@ -0,0 +1,27 @@
+﻿namespace CrystalQuartz.Core.SchedulerProviders
+{
+    using System;
+    using Quartz;
+
+    public class FuncSchedulerProvider : ISchedulerProvider
+    {
+        private readonly Func<IScheduler> _factory;
+
+        public FuncSchedulerProvider(Func<IScheduler> factory)
+        {
+            _factory = factory;
+        }
+
+        public void Init()
+        {
+        }
+
+        public IScheduler Scheduler
+        {
+            get
+            {
+                return _factory.Invoke();
+            }
+        }
+    }
+}
--- src/CrystalQuartz.Owin/AppBuilderExtensions.cs
+++ src/CrystalQuartz.Owin/AppBuilderExtensions.cs
@@ -1,11 +1,43 @@
 ﻿namespace CrystalQuartz.Owin
 {
+    using System;
     using CrystalQuartz.Application;
     using CrystalQuartz.Core.SchedulerProviders;
     using global::Owin;
+    using Quartz;
 
     public static class AppBuilderExtensions
     {
+        public static void UseCrystalQuartz(
+            this IAppBuilder app,
+            IScheduler scheduler)
+        {
+            UseCrystalQuartz(app, scheduler, null);
+        }
+
+        public static void UseCrystalQuartz(
+            this IAppBuilder app,
+            IScheduler scheduler,
+            CrystalQuartzOptions options)
+        {
+            app.UseCrystalQuartz(() => scheduler, options);
+        }
+
+        public static void UseCrystalQuartz(
+            this IAppBuilder app, 
+            Func<IScheduler> schedulerProvider)
+        {
+            UseCrystalQuartz(app, schedulerProvider, null);
+        }
+
+        public static void UseCrystalQuartz(
+            this IAppBuilder app, 
+            Func<IScheduler> schedulerProvider, 
+            CrystalQuartzOptions options)
+        {
+            app.UseCrystalQuartz(new FuncSchedulerProvider(schedulerProvider), options);
+        }
+
         public static void UseCrystalQuartz(
             this IAppBuilder app, 
             ISchedulerProvider scheduleProvider)
