dec0522881bd06ad545c06f7f7dfec95f6795b51
guryanovev <guryanovev@gmail.com>
2018-06-28 21:31:04 +0300

    fixing ga tracking code

--- src/CrystalQuartz.Application.Client/demo/index.ts
+++ src/CrystalQuartz.Application.Client/demo/index.ts
@@ -278,13 +278,14 @@ $$.ajax = function (response) {
 (() => {
     const dataLayer = [];
 
-    window['dataLayer'] = dataLayer;
-
     function gtag(...args){dataLayer.push(args);}
     gtag('js', new Date());
     gtag('config', 'UA-121536364-1');
 
-    var gaJs = document.createElement("script");
+    window['dataLayer'] = dataLayer;
+    window['gtag'] = gtag;
+
+    const gaJs = document.createElement("script");
     gaJs.type = "text/javascript";
     gaJs.src = "https://www.googletagmanager.com/gtag/js?id=UA-121536364-1";
     document.body.appendChild(gaJs);
