8b2dc94c4a9681e62fe28b4e8806ba19e4fb0276
guryanovev <guryanovev@gmail.com>
2018-07-20 13:43:22 +0300

    sync samples

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -10,5 +10,5 @@
 using System.Reflection;
 
 [assembly: AssemblyProductAttribute("CrystalQuartz")]
-[assembly: AssemblyVersionAttribute("6.8.0.0")]
-[assembly: AssemblyFileVersionAttribute("6.8.0.0")]
+[assembly: AssemblyVersionAttribute("6.8.1.0")]
+[assembly: AssemblyFileVersionAttribute("6.8.1.0")]
